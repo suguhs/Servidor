50eddecb96e26a34299a7b2f9d815b9ded851916
Hugo Novo <hugonovo@gmail.com>
2024-10-02 13:18:48 +0200

    9

---
+++ Practica1Unidad2/Ejer9.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Escriba un programa a partir de un número entero generado de forma aleatoria
+indique si es primo. Un número primo es aquel que es divisible por el mismo y la
+unidad
+*/
+
+function esprimo(){
+
+    
+}
+
+?>
