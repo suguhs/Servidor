49bfcb3f4079c00c2acf01b305687cf84ee1764e
Hugo Novo <hugonovo@gmail.com>
2024-10-03 10:30:06 +0200

    todos los ejercicios

---
+++ Practica1Unidad2/Ejer10.php
@@ -0,0 +1,13 @@
+<?php
+/*Generar todos los pares de números formados por combinaciones de dígitos del 1
+al 9, siendo además los dos componentes del par distintos*/
+ 
+
+
+
+for ($i=1; $i<10; $i++) {
+    for ($j=1; $j<10; $j++) {
+      if ($i!=$j) echo "($i,$j)";
+    }
+  }
+?>
--- Practica1Unidad2/Ejer9.php
+++ Practica1Unidad2/Ejer9.php
@@ -4,10 +4,25 @@ Escriba un programa a partir de un número entero generado de forma aleatoria
 indique si es primo. Un número primo es aquel que es divisible por el mismo y la
 unidad
 */
-
-function esprimo($num){
-
-
+function esPrimo($num) {
+    if ($num <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) {
+            return false; 
+        }
+    }
+    return true; 
 }
 
+
+$num = rand(1, 100);
+
+
+if (esPrimo($num)) {
+    echo "$num es primo";
+} else {
+    echo "$num NO es primo";
+}
 ?>
