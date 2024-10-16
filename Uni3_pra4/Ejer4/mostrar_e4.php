39456d8d118e7d01ffe6b241125401e1391580c8
Hugo Novo <hugonovo@gmail.com>
2024-10-16 14:19:22 +0200

    Primer apartado Ejer5

--- Uni3_pra4/Ejer4/mostrar_e4.php
+++ Uni3_pra4/Ejer4/mostrar_e4.php
@@ -1,10 +1,32 @@
 <?php
+include("Ejer4.php");
+// 1. 
+$coche = new Coche("Verde", 1400, 4);
+$coche->añadir_persona(65);
+$coche->añadir_persona(65);
+echo "Color del coche: " . $coche->getColor() . "\n";
+echo "Peso del coche tras añadir las personas: " . $coche->getPeso() . " kg\n";
 
-$vehiculo = new Vehiculo("Negro", 1500);
-echo $vehiculo;
-echo "\n";
-$vehiculo->circular(); 
-$vehiculo->añadir_persona(70); 
+// 2. 
+$coche->repintar("Rojo");
+$coche->añadir_cadenas_nieve(2);
+echo "Color del coche: " . $coche->getColor() . "\n";
+echo "Número de cadenas de nieve: " . $coche->getNumeroCadenasNieve() . "\n";
 
+// 3. 
+$dos_ruedas = new Dos_Ruedas("Negro", 120, 500);
+$dos_ruedas->añadir_persona(80);
+$dos_ruedas->poner_gasolina(20);
+echo "Color de dos ruedas: " . $dos_ruedas->getColor() . "\n";
+echo "Peso de dos ruedas: " . $dos_ruedas->getPeso() . " kg\n";
+
+// 4. 
+$camion = new Camion("Azul", 10000, 2, 10);
+$camion->añadir_remolque(5);
+$camion->añadir_persona(80);
+echo "Color del camión: " . $camion->getColor() . "\n";
+echo "Peso del camión: " . $camion->getPeso() . " kg\n";
+echo "Longitud del camión: " . $camion->getLongitud() . " metros\n";
+echo "Número de puertas del camión: " . $camion->getNumeroPuertas() . "\n";
 
 ?>
