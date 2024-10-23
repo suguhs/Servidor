950083765eaabf6222a6ec2a4062e38283ebeaaf
Hugo Novo <hugonovo@gmail.com>
2024-10-23 14:20:51 +0200

    asd

--- Formulario/Uni3_Pra1/Ejer2/Ejer2.php
+++ Formulario/Uni3_Pra1/Ejer2/Ejer2.php
@@ -1,5 +1,27 @@
 <?php
+if (isset($_GET['Opciones'])) {
+    // Recoger los valores
+    $opciones = $_GET['opciones'];
+    
+    switch(){
+        case "Op1"
+        
+        break;
+        case "Op2"
+
+        break;
+        case "Op3"
+
+        break;
+        case "Op4"
+
+        break;
+        case "Op5"
+        
+        break;
 
 
+    }
+}
 
 ?>
