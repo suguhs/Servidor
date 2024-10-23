edc5a792bcf251ada96b32f931f7ebd4b53b87e8
Hugo Novo <hugonovo@gmail.com>
2024-10-23 13:03:25 +0200

    A

---
+++ Formulario/Uni3_Pra1/Ejer1/Ejer1.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST ["nombre"])){
+$nom=$_GET['nombre'];
+echo "nombre: ".$nom;
+$ape = $_GET['apellidos'];
+echo "apellido: ".$ape;
+}
+
+
+?>
---
+++ Formulario/Uni3_Pra1/Ejer1/Ejer1f.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Verificación</title>
+</head>
+<body>
+    <h1>Calculadora</h1>
+    <form action="Ejer1.php" method="get">
+        <label for="nombre">A:</label>
+        <input type="Number" id="n1" name="n1" required><br><br>
+
+        <label for="apellidos">B:</label>
+        <input type="Number" id="n2" name="n2" required><br><br>
+
+        <input type="submit" value="Verificar">
+    </form>
+</body>
+</html>
